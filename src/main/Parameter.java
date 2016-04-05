04b5b81c4ff70081a0ef6cb62d8b6e4fd223fbd1
Yuichi Kuroda <pochi.black@gmail.com>
2016-04-05 10:01:33 +0900

    Change class name

--- src/main/Parameter.java
+++ src/main/Parameter.java
@@ -10,12 +10,19 @@ import twitter4j.util.*;
 *    >>> java -cp classes/ pochi.examples.NicosParameter
 *
 * 2. jarを組み込む
+*
+*    lib/に組み込んで上のパスを入れようとしている
+*
 * 3. 引数呼び出す
+*
+*
+*
 * 4. まねする
 *
 */
-public class NicosParameter {
+public class Parameter {
   public static void main(String[] args) {
+
     System.out.println("hoge moge");
   }
 }

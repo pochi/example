b532985603e86133981bb7ac5cb998f539ae8500
Yuichi Kuroda <pochi.black@gmail.com>
2016-04-05 09:34:05 +0900

    First commit

---
+++ src/main/NicosParameter.java
@@ -0,0 +1,17 @@
+/*
+*
+* 1. Javaで実行する
+*
+*    >>> javac src/main/NicosParameter.java
+*    >>> java -cp src/main NicosParameter
+*
+* 2. jarを組み込む
+* 3. 引数呼び出す
+* 4. まねする
+*
+*/
+public class NicosParameter {
+  public static void main(String[] args) {
+    System.out.println("hoge moge");
+  }
+}
